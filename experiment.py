0af7495264ccb768b0d1503e9131f181114e263d
yazan <1210145@student.birzeit.edu>
2024-08-23 22:24:27 +0300

    Added a new module

--- experiment.py
+++
@@ -1,9 +0,0 @@
-from datetime import *
-
-import Test
-from ConsolePrinter import ConsolePrinter
-from Patient import Patient
-from Test_type import Test_type
-
-Patient.import_records()
-print(ConsolePrinter.print_record_file())
